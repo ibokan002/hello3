fd2ab4e108d7338c4a82ce11bbc22d807302d3c2
ibokan002 <ibokan002@163.com>
2012-11-29 15:40:54 +0800

    1232131

---
+++ hhh/hhh/Nstime.h
@@ -0,0 +1,13 @@
+//
+//  Nstime.h
+//  hhh
+//
+//  Created by Ibokan on 12-11-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Nstime : NSObject
+
+@end
---
+++ hhh/hhh/Nstime.m
@@ -0,0 +1,13 @@
+//
+//  Nstime.m
+//  hhh
+//
+//  Created by Ibokan on 12-11-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "Nstime.h"
+
+@implementation Nstime
+
+@end
